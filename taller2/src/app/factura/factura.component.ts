fe709b6e0a0e716fd6a977c5f22b511fb0bea61b
Adriana Marcela Giraldo <iyerladis15@gmail.com>
2021-07-21 11:31:39 -0500

    terminado

--- taller2/src/app/factura/factura.component.ts
+++ taller2/src/app/factura/factura.component.ts
@@ -10,30 +10,34 @@ import { Item } from '../clases/items';
 export class FacturaComponent implements OnInit {
 
   consecutivoF: number = 0
-  facturas: String[]
+  facturas: String[] 
+  items: String[]
   item = ''
   
   cajaFactura: String = '';
 
   constructor() {
     this.facturas = []
+    this.items =[]
 
    }
 
-  anadirFactura(item: String) {
-    if(item == "") {
-      alert("DEBE DIGITAR UNA FACTURA POR FAVOR!")
+  anadirFactura(item: String,tipo: string) {
+    if(tipo == "factura") {
+      this.consecutivoF = this.consecutivoF + 1
+      this.facturas.push(item)
 
     } else {
       this.consecutivoF = this.consecutivoF + 1
-      this.facturas.push(item)
+      this.items.push(item)
 
     }
     
 
   }
 
-  removerFactura() {
+  removerFactura(tipo: String) { 
+    if (tipo=="factura"){
     if(this.facturas.length == 0) {
       alert("NO HAY FACTURAS PARA ELIMINAR!")
 
@@ -41,8 +45,20 @@ export class FacturaComponent implements OnInit {
       this.facturas.splice(this.facturas.length - 1, 1)
 
     }
+  } else {
+
+    if(this.items.length == 0) {
+      alert("NO HAY ITEMS PARA ELIMINAR!")
+
+    } else {
+      this.items.splice(this.items.length - 1, 1)
+
+    }
 
   }
+  
+  }
+
 
   ngOnInit(): void {
   }
